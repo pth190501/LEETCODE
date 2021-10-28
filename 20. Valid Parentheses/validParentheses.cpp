0063c86a6487d843e2185729f5a6bb9691ebbbce
Hai <pth190501@gmail.com>
2021-10-28 19:23:48 +0700

    commit

---
+++ 20. Valid Parentheses/validParentheses.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        char c;
+        stack<char >st;
+        for(int i=0;i<s.size();i++){
+            
+            switch (s[i]){
+                case '(':
+                    st.push(s[i]);
+                    break;
+                case '{':
+                    st.push(s[i]);
+                    break;
+                case '[':
+                    st.push(s[i]);
+                    break;
+                case ')':
+                    if(st.empty()){
+                        return false;
+                    }
+                    if(st.top()!='('){
+                        return false;
+                    }
+                    st.pop();
+                    break;
+                case '}':
+                    if(st.empty()){
+                        return false;
+                    }
+                    if(st.top()!='{' ){
+                        return false;
+                    }
+                    st.pop();
+                    break;
+                case ']':
+                    if(st.empty()){
+                        return false;
+                    }
+                    if(st.top()!='['){
+                        return false;
+                    }
+                    st.pop();
+                    break;
+                default :
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
