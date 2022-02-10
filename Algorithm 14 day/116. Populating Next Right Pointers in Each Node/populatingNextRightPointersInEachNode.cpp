fcfb71d9e13dbe72a6443d9e8efd80a6d0eabf9f
hai <pth190501@gmail.com>
2022-02-10 11:11:16 +0700

    day 9

---
+++ Algorithm 14 day/116. Populating Next Right Pointers in Each Node/populatingNextRightPointersInEachNode.cpp
@@ -0,0 +1,30 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+		if(!root) return root;
+		if(!root->right) return root;
+		root->left->next = root->right;
+        if(root->left && root->next) root->right->next = root->next->left;
+		auto x = connect(root->left);
+		auto y = connect(root->right);
+		return root;
+    }
+};
